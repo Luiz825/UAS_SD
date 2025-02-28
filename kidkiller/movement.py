acaeaae1e09dcea211be26b707d4a65314713b5e
Luiz825 <luiz.gonzalez6194@gmail.com>
2025-02-27 16:33:00 -0800

    Update movement.py

--- kidkiller/movement.py
+++ kidkiller/movement.py
@@ -26,16 +26,17 @@ def __zumba(xv, yv, zv, yaw):
 def __hit_the_gas(xa, ya, za, yaw):
     ln.the_connection.mav.send(mavutil.mavlink.MAVLink_set_position_target_local_ned_message(0, ln.the_connection.target_system, ln.the_connection.target_component, mavutil.mavlink.MAV_FRAME_LOCAL_NED, 3527, 0, 0, 0, 0, 0, 0, xa, ya, za, yaw, 0))
 
-def hold_until(t_x, t_y, t_z): 
+def hold_until(t_x, t_y, t_z, tol = 0.5):
     #madagascar(xv = 0, yv = 0, zv = 0)       
     while True:        
-        msg = ls.wait_4_msg("GLOBAL_POSITION_INT")
-        x = msg.lat
-        y = msg.lon
-        z = msg.alt
-        print(f"Current Position: Lat {x/1e7}, Lon {y/1e7}, Alt {z/1000}m")
-        print(f"Target Position: Lat {t_x/1e7}, Lon {t_y/1e7}, Alt {t_z/1000}m")
-        if(abs(t_x - x) < 0.5 and abs(t_y - y) < 0.5 and abs(t_z - z) < 0.5):
+        msg = ls.wait_4_msg("LOCAL_POSITION_NED")
+        x = msg.x
+        y = msg.y
+        z = msg.z
+        print(f"Current Position: x {x:.2f}, y {y:.2f}, z {z:.2f}m")
+        print(f"Target Position: x {t_x:.2f}, y {t_y:.2f}, z {t_z:.2f}m")
+        if(abs(t_x - x) < tol and abs(t_y - y) < tol and abs(t_z - z) < tol):
+            print(ls.wait_4_ack())
             break
 
 def hold_until_v(xv, yv, zv):        
