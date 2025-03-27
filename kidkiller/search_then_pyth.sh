ab217f7255c57477ee69b6897ba20869001cac54
Sophia Garcia <gasophia2@gmail.com>
2025-03-27 10:26:57 -0700

    code edits

--- kidkiller/search_then_pyth.sh
+++ kidkiller/search_then_pyth.sh
@@ -1,14 +1,37 @@
 #!/bin/bash
 
-echo "waiting for /dev/ttyUSB0..." >> /
+STATUS="Activate drone detection! :0"
+TIMESTAMP=$(date "+%c")
+declare -i lim=3
 
-while [ ! -e /dev/ttyUSB0 ]; do
+xterm -e "echo 'Hello, this is a message!'; echo 'Script is running...';"
+
+for ((i=1; i<=$lim; i++))
+do    
+
+    TIMESTAMP=$(date "+%c")
+
+    xterm -e "echo '$STATUS'; echo '$TIMESTAMP'; exec bash" &
+
+    echo "$STATUS\n$TIMESTAMP" >>  rp5_login_state.txt     
+
+    if [ ! -e /dev/ttyUSB0 ] && [ "$i" -eq $lim ]; then
+        STATUS="Was not able to find /dev/ttyUSB0 sry :("          
+        exit 1
+    elif [ -e /dev/ttyUSB0 ] && [ "$i" -le $lim ]; then
+        STATUS="Found /dev/ttyUSB0! :>"
+    else 
+        STATUS="waiting for /dev/ttyUSB0..."
+    fi     
     sleep 1
-done
+done    
+echo -e "/dev/ttyUSB0 found! Running python..."
+echo "/dev/ttyUSB0 found! Running python..." >> rp5_login_state.txt        
+source /media/cece/DuelData/academic/SDSU/SP2025/COMPE492/UAS_SD/kidkiller/myenv_pymavlink/bin/activate #change to local path
+# enviornment sett on the rapberry pi
+python3 /media/cece/DuelData/academic/SDSU/SP2025/COMPE492/UAS_SD/kidkiller/indep_thought.py #change for local path
 
-echo "/dev/ttyUSB0 found! Running python..."
-source # enviornment sett on the rapberry pi
-python3 # insert path here after pulling from GitHub on raspberry pi
+# insert path here after pulling from GitHub on raspberry pi
 
 
 #folllow steps to set up on rp5 so that the script begins when 
