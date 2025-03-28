8c91b01cb036e35007d12c522d5ddd902667dca6
Sophia Garcia <gasophia2@gmail.com>
2025-03-28 10:23:33 -0700

    new sh file for rp5 on startup

--- autonomous_code/search_then_pyth.sh
+++ autonomous_code/search_then_pyth.sh
@@ -2,29 +2,32 @@
 
 STATUS="Activate drone detection! :0"
 TIMESTAMP=$(date "+%c")
-declare -i lim=3
+declare -i lim=5
 
-xterm -e "echo 'Hello, this is a message!'; echo 'Script is running...';"
+    echo -e "$STATUS\n$TIMESTAMP"
+    echo "$STATUS\n$TIMESTAMP" >> rp5_login_state.txt
 
 for ((i=1; i<=$lim; i++))
 do    
+    sleep 1
+    if [ ! -e /dev/ttyUSB0 ] && [ "$i" -eq $lim ]; then
+        STATUS="Was not able to find /dev/ttyUSB0 sry :("          
+    elif [ -e /dev/ttyUSB0 ] && [ "$i" -le $lim ]; then
+        STATUS="Found /dev/ttyUSB0...! :>"
+    else 
+        STATUS="waiting for /dev/ttyUSB0..."
+    fi 
 
     TIMESTAMP=$(date "+%c")
 
-    xterm -e "echo '$STATUS'; echo '$TIMESTAMP'; exec bash" &
+    echo -e "$STATUS\n$TIMESTAMP"
 
-    echo "$STATUS\n$TIMESTAMP" >>  rp5_login_state.txt     
-
-    if [ ! -e /dev/ttyUSB0 ] && [ "$i" -eq $lim ]; then
-        STATUS="Was not able to find /dev/ttyUSB0 sry :("          
-        exit 1
-    elif [ -e /dev/ttyUSB0 ] && [ "$i" -le $lim ]; then
-        STATUS="Found /dev/ttyUSB0! :>"
-    else 
-        STATUS="waiting for /dev/ttyUSB0..."
-    fi     
-    sleep 1
+    echo "$STATUS\n$TIMESTAMP" >> rp5_login_state.txt                
 done    
+
+if [ "$STATUS" = "Was not able to find /dev/ttyUSB0 sry :(" ]; then  
+    exit 0
+fi
 echo -e "/dev/ttyUSB0 found! Running python..."
 echo "/dev/ttyUSB0 found! Running python..." >> rp5_login_state.txt        
 source /media/cece/DuelData/academic/SDSU/SP2025/COMPE492/UAS_SD/kidkiller/myenv_pymavlink/bin/activate #change to local path
@@ -32,7 +35,6 @@ source /media/cece/DuelData/academic/SDSU/SP2025/COMPE492/UAS_SD/kidkiller/myenv
 python3 /media/cece/DuelData/academic/SDSU/SP2025/COMPE492/UAS_SD/kidkiller/indep_thought.py #change for local path
 
 # insert path here after pulling from GitHub on raspberry pi
-
 
 #folllow steps to set up on rp5 so that the script begins when 
 #rp on rebbot
