9dbfe60b89bc4caf8e5d7dbe2757aea238a0c57c
Sophia Garcia <gasophia2@gmail.com>
2025-04-10 16:02:44 -0700

    added servo movement after debug; requires  more hardware

--- autonomous_code/drone_move/learning.py
+++ autonomous_code/drone_move/learning.py
@@ -1,5 +1,5 @@
 from pymavlink import mavutil 
 
-#the_connection = mavutil.mavlink_connection('/dev/ttyUSB0', baud = 57600) # keep permenant! not changeable! while using uart mod with GND
-the_connection = mavutil.mavlink_connection('udp:localhost:14551') # keep for simulations! need to add to sim inputs when simming with 'output add 127.0.0.1:14551' command
+the_connection = mavutil.mavlink_connection('/dev/ttyUSB0', baud = 57600) # keep permenant! not changeable! while using uart mod with GND
+#the_connection = mavutil.mavlink_connection('udp:localhost:14551') # keep for simulations! need to add to sim inputs when simming with 'output add 127.0.0.1:14551' command
 
--- autonomous_code/drone_move/servo_testing.py
+++ autonomous_code/drone_move/servo_testing.py
@@ -1,14 +1,23 @@
 import learning as ln
 from drone_class import Drone
-from time import time
+import time
 
 if __name__ == "__main__":
     drone = Drone()
-    start = time.time()
-    while True:
-        drone.move_servo(6, 1500)
-        time.sleep(5)
-        drone.move_servo(6, 1100)
-        time.sleep(5)
-        if time.time() - start == 60:
-            break
+    msg = drone.wait_4_msg(str_type="HEARTBEAT", block=True)
+    if msg:
+        print(msg)
+        start = time.time()
+        for i in range (8, 11):
+            print(f"Check servo: {i}")
+            drone.move_servo(i, 1000)
+            msg_0 = drone.wait_4_msg(str_type="COMMAND_ACK", time_out_sess=4, attempts=2)
+            if msg_0:
+                print(msg_0)
+            time.sleep(0.5)
+            drone.move_servo(i, 1500)
+            msg_25 = drone.wait_4_msg(str_type="COMMAND_ACK", time_out_sess=4, attempts=2)
+            if msg_25:
+                print(msg_25)
+            time.sleep(0.5)                        
+        print("4 seconds")
