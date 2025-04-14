0407cf4f8a2941b4986128f2aa22fc2c9ae50934
Sophia Garcia <gasophia2@gmail.com>
2025-04-13 23:50:55 -0700

    channges in how default comms go

--- autonomous_code/drone_move/log_pos_drone.py
+++ autonomous_code/drone_move/log_pos_drone.py
@@ -4,8 +4,8 @@ import sys
 import time
 from pymavlink import mavutil 
 
-the_connection = mavutil.mavlink_connection('/dev/ttyUSB0', baud = 57600) # keep permenant! not changeable! while using uart mod with GND
-#the_connection = mavutil.mavlink_connection('udp:localhost:14551') # keep for simulations! need to add to sim inputs when simming with 'output add 127.0.0.1:14551' command
+conn = '/dev/ttyUSB0' # keep permenant! not changeable! while using uart mod with GND
+# the default'udp:localhost:14551 keep for simulations! need to add to sim inputs when simming with 'output add 127.0.0.1:14551' command
 
 async def main(drone):
     loop = asyncio.get_event_loop()    
