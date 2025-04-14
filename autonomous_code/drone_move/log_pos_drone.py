e8222e10a3ed8b5438d5d0045d0c87535efc765f
Sophia Garcia <gasophia2@gmail.com>
2025-04-13 21:47:14 -0700

    specific to the rp5 also

--- autonomous_code/drone_move/log_pos_drone.py
+++ autonomous_code/drone_move/log_pos_drone.py
@@ -2,19 +2,23 @@ import drone_class as dc
 import asyncio
 import sys
 import time
+from pymavlink import mavutil 
+
+the_connection = mavutil.mavlink_connection('/dev/ttyUSB0', baud = 57600) # keep permenant! not changeable! while using uart mod with GND
+#the_connection = mavutil.mavlink_connection('udp:localhost:14551') # keep for simulations! need to add to sim inputs when simming with 'output add 127.0.0.1:14551' command
 
 async def main(drone):
     loop = asyncio.get_event_loop()    
 
     await asyncio.gather(
-        drone.log_test_time_ed(loop_time_min=30) #30 minutes
+        drone.log_test_time_ed(loop_time_min=3) #30 minutes
     )
 
 # this is testing grounds for different paths and options
 #a.mode_activate('GUIDED')
 if __name__ == '__main__':
     time.sleep(2)
-    with open("/home/pi/STORK_TEST_LOG_FAILS.txt", "w") as f:
+    with open("/home/pi/STORK_TEST_LOG.txt", "w") as f:
         # Redirect stdout to the file
         original_stdout = sys.stdout  # Save original stdout
         sys.stdout = f
@@ -31,7 +35,7 @@ if __name__ == '__main__':
         f.flush()
         f.close() 
     
-    print(f"done or did nt work lol")
+    print(f"done or did not work lol")
 
 
 
